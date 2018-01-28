19f8503d0a5db9ee0604b9d8c0d8b688e6cfb536
Monika Williams <monika.m.williams@gmail.com>
2018-01-28 08:53:12 +0000

    automatically backed up by learn

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,13 +1,17 @@
 class LineItemsController < ApplicationController
+  before_action :authentication_required
+
   def create
-      if current_user.current_cart != nil
-        @current_cart = current_user.current_cart
-      else
-        current_user.current_cart = current_user.carts.create(user_id: current_user.id)
-        @current_cart = current_user.current_cart
-      end
-      line_item = @current_cart.add_item(params[:item_id])
-      line_item.save
-      redirect_to cart_path(@current_cart)
+    user = current_user
+    if user.current_cart
+      user.current_cart.add_item(params[:item_id])
+    else
+      user.current_cart = user.carts.create
+      user.current_cart.add_item(params[:item_id])
     end
+    user.current_cart.save
+    user.save
+
+    redirect_to cart_path(user.current_cart)
+  end
 end
