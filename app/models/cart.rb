b1cdd132a421c8a89213547f2e84faf26b98f856
Monika Williams <monika.m.williams@gmail.com>
2018-01-28 08:55:52 +0000

    automatically backed up by learn

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,23 +1,33 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
   belongs_to :user
-    has_many :line_items
-    has_many :items, through: :line_items
 
-    def add_item(item_id)
-      line_item = self.line_items.find_by(item_id: item_id)
-      if line_item
-        line_item.quantity += 1
-      else
-        line_item = self.line_items.build(item_id: item_id)
-      end
-      line_item
+  def total
+    total = 0
+    self.items.each do |item|
+      quantity = self.line_items.find_by(item_id: item.id).quantity
+      total += item.price * quantity
     end
+    total
+  end
 
-    def total
-      total = 0
-      self.line_items.each do |line_item|
-        total += (line_item.item.price * line_item.quantity)
-      end
-      total
+  def add_item(item_id)
+    if existing_line_item = self.line_items.find_by(item_id: item_id)
+      existing_line_item.duplicate_items_in_cart
+      existing_line_item
+    else
+      self.line_items.build(item_id: item_id)
     end
+  end
+
+  def cart_checkout
+    self.items.each do |item|
+      quantity = self.line_items.find_by(item_id: item.id).quantity
+      item.inventory -= quantity
+      item.save
+    end
+    self.status = "submitted"
+    self.save
+  end
 end
